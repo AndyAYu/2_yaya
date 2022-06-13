9050d84770aec57204875605e3ded6b06a1b65dd
AndyAYu <andyhkgw@gmail.com>
2022-06-13 12:38:49 -0500

    deleted all user logic

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,5 @@
 
 ActiveRecord::Schema.define(version: 2022_06_09_022102) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "email", null: false
-    t.string "session_token", null: false
-    t.string "password_digest", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["session_token"], name: "index_users_on_session_token", unique: true
-  end
 
 end
