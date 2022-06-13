a9b09f80261b14c2c975dbcc23e7ae8ac8f9b61a
AndyAYu <andyhkgw@gmail.com>
2022-06-13 14:52:39 -0500

    working dropdown/select menu

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_09_022102) do
-
+ActiveRecord::Schema.define(version: 0) do
 
 end
