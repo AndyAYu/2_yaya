c12244baa96a65ac2b1ae8a322f1babf85579e2b
AndyAYu <andyhkgw@gmail.com>
2022-06-13 14:20:25 -0500

    functional dropdown mention with changing states

--- 2_yaya/src/App.js
+++ 2_yaya/src/App.js
@@ -2,16 +2,29 @@ import logo from './logo.svg';
 import * as React from 'react';
 
 const App = () => {
+  const options = [
+    {label: 'Sky' , value: 'sky' },
+    {label: 'Land' , value: 'land' },
+    {label: 'Sea' , value: 'sea' },
+  ]
+  const [value, setValue] = React.useState('sky');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <div>
       <label>
         What animals should we see?
-        <select>
-          <option value='Sky'>Sky</option>
-          <option value='Land'>Land</option>
-          <option value='Sea'>Sea</option>
+        <select value={value} onChange={handleChange}>
+          {options.map((option) => (
+            <option value={option.value}>{option.label}</option>
+          ))}
         </select>
       </label>
+
+      <p>We will see {value} animals!</p>
     </div>
   );
 }
